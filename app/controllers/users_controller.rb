750b665e0dc4a0869bfb710f57fb73bb03eab251
Yuji.Yamaguchi <ieirireon@gmail.com>
2019-09-22 14:52:32 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,29 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = '新規ユーザ登録をしました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザ登録が出来ませんでした。'
+      render :new
+    end
   end
 
   def edit
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
