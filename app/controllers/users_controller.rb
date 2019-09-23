da036633f7067cc3c02b6b8840b4842c975473eb
Yuji.Yamaguchi <ieirireon@gmail.com>
2019-09-23 08:59:28 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, except: [:new, :create]
   
-  def index
-    @users = User.order(id: :asc).page(params[:page]).per(10)
-  end
+#  不要の為  
+#  def index
+#    @users = User.order(id: :asc).page(params[:page]).per(10)
+#  end
 
-  def show
-    @user = User.find(params[:id])
-  end
+#  def show
+#    @user = User.find(params[:id])
+#  end
 
   def new
     @user = User.new
@@ -18,16 +19,13 @@ class UsersController < ApplicationController
     
     if @user.save
       flash[:success] = '新規ユーザ登録をしました。'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザ登録が出来ませんでした。'
       render :new
     end
   end
 
-  def edit
-  end
-  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
